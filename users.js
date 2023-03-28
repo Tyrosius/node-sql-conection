5759f04bb361afbfc595e6a1ceb95ccbffeeeb0c
Tyrosius <florian.schoof@gmx.de>
2023-03-28 11:53:32 +0200

    added "users GET"

--- users.js
+++ users.js
@@ -3,4 +3,15 @@ const pool = require('./db');
 
 const users = Router();
 
+users.get('/', async (req, res) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM users');
+        res.json({ data: rows })
+    } catch (e) {
+        res.sendStatus(404)
+    }
+
+
+})
+
 module.exports = users;
