139afbfd47d973b542f7cb7057e486bf096dba33
Tyrosius <florian.schoof@gmx.de>
2023-03-28 13:11:09 +0200

    added "users GET by id"

--- users.js
+++ users.js
@@ -12,4 +12,12 @@ users.get('/', async (req, res) => {
     }
 })
 
+users.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const { rows } = await pool.query('SELECT * FROM users WHERE id=$1', [id])
+        res.json({ data: rows })
+    } catch (e) { res.sendStatus(404) }
+})
+
 module.exports = users;
