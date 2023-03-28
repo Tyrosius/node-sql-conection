6cc2c8411399b5a7c4fd63303b63823f998c040e
Tyrosius <florian.schoof@gmx.de>
2023-03-28 14:51:34 +0200

    added some validation

--- orders.js
+++ orders.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const pool = require('./db');
+const { body, validationResult } = require('express-validator');
 
 const orders = Router();
 
@@ -20,23 +21,37 @@ orders.get('/:id', async (req, res) => {
     } catch (e) { res.sendStatus(404) }
 })
 
-orders.post('/', async (req, res) => {
-    const { price, date, user_id } = req.body;
-    try {
-        const { rows } = await pool.query('INSERT INTO orders (price, date, user_id) VALUES ($1, $2, $3) RETURNING *', [price, date, user_id]);
-        res.json({ data: rows })
-    } catch (e) { res.sendStatus(403) }
-})
+orders.post('/',
+    body('price').isFloat({ min: 0 })/* add locale options für z.B.(3,5) */,
+    body('date').isISO8601(),
+    body('user_id').isInt()/* isIn([vorhandene ids aus users]) */,
+    async (req, res) => {
 
-orders.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    /* TODO check for data to be changed */
-    const { price } = req.body;
-    try {
-        const { rows } = await pool.query('UPDATE orders SET price=$1 WHERE id=$2 RETURNING *', [price, id]);
-        res.json({ data: rows })
-    } catch (e) { res.sendStatus(403) }
-})
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) { return res.status(400).json({ errors: validationErrors.array() }) }
+
+        const { price, date, user_id } = req.body;
+        try {
+            const { rows } = await pool.query('INSERT INTO orders (price, date, user_id) VALUES ($1, $2, $3) RETURNING *', [price, date, user_id]);
+            res.json({ data: rows })
+        } catch (e) { res.sendStatus(403) }
+    })
+
+orders.put('/:id',
+    body('price').isFloat({ min: 0 }),
+    async (req, res) => {
+
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) { return res.status(400).json({ errors: validationErrors.array() }) }
+
+        const { id } = req.params;
+        /* TODO check for data to be changed */
+        const { price } = req.body;
+        try {
+            const { rows } = await pool.query('UPDATE orders SET price=$1 WHERE id=$2 RETURNING *', [price, id]);
+            res.json({ data: rows })
+        } catch (e) { res.sendStatus(403) }
+    })
 
 orders.delete('/:id', async (req, res) => {
     const { id } = req.params;
--- users.js
+++ users.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const pool = require('./db');
+const { body, validationResult } = require('express-validator');
 
 const users = Router();
 
@@ -20,23 +21,38 @@ users.get('/:id', async (req, res) => {
     } catch (e) { res.sendStatus(404) }
 })
 
-users.post('/', async (req, res) => {
-    const { first_name, last_name, age, active } = req.body;
-    try {
-        const { rows } = await pool.query('INSERT INTO users (first_name, last_name,age,active) VALUES ($1,$2,$3,$4) RETURNING *', [first_name, last_name, age, active]);
-        res.json({ data: rows })
-    } catch (e) { res.sendStatus(403) }
-})
+users.post('/',
+    body('first_name').isLength({ min: 3, max: undefined }),
+    body('last_name').isLength({ min: 3, max: undefined }),
+    body('age').isInt(),
+    body('active').isBoolean({ loose: true }),
+    async (req, res) => {
 
-users.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    /* TODO check for data to be changed */
-    const { first_name } = req.body;
-    try {
-        const { rows } = await pool.query('UPDATE users SET first_name=$1 WHERE id=$2 RETURNING *', [first_name, id]);
-        res.json({ data: rows })
-    } catch (e) { res.sendStatus(403) }
-})
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) { return res.status(400).json({ errors: validationErrors.array() }) }
+
+        const { first_name, last_name, age, active } = req.body;
+        try {
+            const { rows } = await pool.query('INSERT INTO users (first_name, last_name,age,active) VALUES ($1,$2,$3,$4) RETURNING *', [first_name, last_name, age, active]);
+            res.json({ data: rows })
+        } catch (e) { res.sendStatus(403) }
+    })
+
+users.put('/:id',
+    body('first_name').isLength({ min: 3, max: undefined }),
+    async (req, res) => {
+
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) { return res.status(400).json({ errors: validationErrors.array() }) }
+
+        const { id } = req.params;
+        /* TODO check for data to be changed */
+        const { first_name } = req.body;
+        try {
+            const { rows } = await pool.query('UPDATE users SET first_name=$1 WHERE id=$2 RETURNING *', [first_name, id]);
+            res.json({ data: rows })
+        } catch (e) { res.sendStatus(403) }
+    })
 
 users.delete('/:id', async (req, res) => {
     const { id } = req.params;
